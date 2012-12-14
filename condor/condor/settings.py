a74d9dd2abce5bae9f41b372ff2df783a89e6a6f
Sam Dunster <me@sdunster.com>
2012-12-14 23:04:55 +1100

    Fix grappelli and move db again

--- condor/condor/settings.py
+++ condor/condor/settings.py
@@ -14,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_ROOT, '../db.sqlite'),
+        'NAME': os.path.join(PROJECT_ROOT, '../../db.sqlite'),
     }
 }
 
@@ -113,8 +113,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.admin',
     'grappelli',
+    'django.contrib.admin',
     'south',
     
     'members',
